f2d3740863d9a141da988574030725003bbc3a62
iih <iih@iih.me>
2013-08-13 20:32:02 +0400

    Created a railsa pplication and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '71e071a67b187c649a3ecf645aa1f93d50047eae4b1ee2801215d77cfab73f85401a766f0cc5357eda342e4e40fbf5e3d992ea9c70db21fdfff2e96071ed5555'
